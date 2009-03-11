4d4299308d0a556604a0c18fe8084ca4a3f93723
Mikhail Grigoriev <mgrigoriev@gmail.com>
2009-03-11 23:23:21 +0300

    Rename custom 'site_media_url' context processor to 'host', remove media_url from it because it exists already

--- opentodo/context_processors.py
+++ opentodo/context_processors.py
@@ -1,7 +1,3 @@
-def site_media_url(request):
+def host(request):
     from django.conf import settings
-    return {'MEDIA_URL': settings.MEDIA_URL, 'HOST': request.get_host()}
-#    return {
-#		'SITE_MEDIA_URL': settings.SITE_MEDIA_URL,
-#		'PROJECT_PATH': settings.PROJECT_PATH,
-#		'MEDIA_URL': settings.MEDIA_URL}
+    return {'HOST': request.get_host()}
--- opentodo/settings.py
+++ opentodo/settings.py
@@ -78,7 +78,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
-    'context_processors.site_media_url'
+    'context_processors.host'
 )
 
 AUTHENTICATION_BACKENDS = (
