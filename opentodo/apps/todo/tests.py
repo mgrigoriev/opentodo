3735463a2787987cbb668d1216dafe3ee71e2f97
Mikhail Grigoriev <mgrigoriev@gmail.com>
2009-03-17 20:45:42 +0300

    Use reverse method instead of hardcoded urls in tests. Add '500.html' template

--- opentodo/apps/todo/tests.py
+++ opentodo/apps/todo/tests.py
@@ -3,9 +3,11 @@
 from django.test import TestCase
 from django.test.client import Client
 from django.conf import settings
-
+from django.core.urlresolvers import reverse
+import urls
 from django.contrib.auth.models import User
 from todo.models import Status, Task, Project, Comment
+
 
 class TestViews(TestCase):
     fixtures = ['todo.json']
@@ -37,23 +39,23 @@ class TestViews(TestCase):
         self.failUnless(rs)
 
         urls = [
-                    ("/", 302),
-                    ("/tasks/new/", 200),
-                    ("/tasks/1/comments/new/", 302),
-                    ("/tasks/delete_comment/1/", 302),
-                    ("/tasks/1/", 200),
-                    ("/tasks/1/edit/", 200),
-                    ("/tasks/1/to_accepted/", 302),
-                    ("/tasks/1/to_done/", 302),
-                    ("/tasks/1/to_checked/", 302),
-                    ("/tasks/1/to_new/", 302),
-                    ("/tasks/1/delete/", 302),
-                    ("/projects/", 200),
-                    ("/projects/new/", 200 ),
-                    ("/projects/2/", 200),
-                    ("/projects/2/edit/", 200),
-                    ("/json/project_users/", 200),
-                    ("/projects/2/delete/", 302),
+                    ('/',                                    302),
+                    (reverse('add_task'),                    200),
+                    (reverse('add_comment', args=(1,)),      302),
+                    (reverse('del_comment', args=(1,)),      302),
+                    (reverse('task_details', args=(1,)),     200),
+                    (reverse('edit_task', args=(1,)),        200),
+                    (reverse('task_to_accepted', args=(1,)), 302),
+                    (reverse('task_to_done', args=(1,)),     302),
+                    (reverse('task_to_checked', args=(1,)),  302),
+                    (reverse('task_to_new', args=(1,)),      302),
+                    (reverse('delete_task', args=(1,)),      302),
+                    (reverse('projects_list'),               200),
+                    (reverse('add_project'),                 200 ),
+                    (reverse('project_details', args=(2,)),  200),
+                    (reverse('edit_project', args=(2,)),     200),
+                    (reverse('json_project_users'),          200),
+                    (reverse('delete_project', args=(2,)),   302),
                ]
 
         for url in urls:
@@ -68,23 +70,23 @@ class TestViews(TestCase):
         self.failUnless(rs)
         
         # правильные данные (ожидаем 302 - редирект на просмотр задачи)
-        rs = self.c.post("/tasks/new/", {'project':'1', 'title':'New task', 'assigned_to':'1', 'deadline':'2008-12-12'})
+        rs = self.c.post(reverse('add_task'), {'project':'1', 'title':'New task', 'assigned_to':'1', 'deadline':'2008-12-12'})
         self.assertEquals(rs.status_code, 302)
 
         # правильные данные, ответственный - пользователь без email (ожидаем 302 - редирект на просмотр задачи)
-        rs = self.c.post("/tasks/new/", {'project':'1', 'title':'One more', 'assigned_to':'2', 'deadline':'2008-12-12'})
+        rs = self.c.post(reverse('add_task'), {'project':'1', 'title':'One more', 'assigned_to':'2', 'deadline':'2008-12-12'})
         self.assertEquals(rs.status_code, 302)
 
         # правильные данные, ответственный не назначен (ожидаем 302 - редирект на просмотр задачи)
-        rs = self.c.post("/tasks/new/", {'project':'1', 'title':'Not assigned task'})
+        rs = self.c.post(reverse('add_task'), {'project':'1', 'title':'Not assigned task'})
         self.assertEquals(rs.status_code, 302)
 
         # неверный формат даты (ожидаем 200)
-        rs = self.c.post("/tasks/new/", {'project':'1', 'title':'New task', 'deadline': 'invalid_date'})
+        rs = self.c.post(reverse('add_task'), {'project':'1', 'title':'New task', 'deadline': 'invalid_date'})
         self.assertEquals(rs.status_code, 200)
         
         # не введена задача (ожидаем 200)
-        rs = self.c.post("/tasks/new/", {'project':'1', 'title':''})
+        rs = self.c.post(reverse('add_task'), {'project':'1', 'title':''})
         self.assertEquals(rs.status_code, 200)
 
     """
@@ -95,7 +97,7 @@ class TestViews(TestCase):
         self.failUnless(rs)
         
         # правильные данные (ожидаем 302 - редирект на просмотр задачи)
-        rs = self.c.post("/tasks/1/edit/", {'project':'1', 'title':'Edited task', 'assigned_to':'1'})
+        rs = self.c.post(reverse('edit_task', args=(1,)), {'project':'1', 'title':'Edited task', 'assigned_to':'1'})
         self.assertEquals(rs.status_code, 302)
 
     """
@@ -106,9 +108,9 @@ class TestViews(TestCase):
         self.failUnless(rs)
 
         # правильные данные (ожидаем 302)
-        rs = self.c.post("/projects/new/", {'title':'New project'})
+        rs = self.c.post(reverse('add_project'), {'title':'New project'})
         self.assertEquals(rs.status_code, 302)
 
         # не введено название проекта
-        rs = self.c.post("/projects/new/", {'title':''})
+        rs = self.c.post(reverse('add_project'), {'title':''})
         self.assertEquals(rs.status_code, 200)
