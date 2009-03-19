a74a20efb7c923c161061fb939c749d5702196a4
Mikhail Grigoriev <mgrigoriev@gmail.com>
2009-03-19 21:48:15 +0300

    Remove admin.py

--- opentodo/apps/todo/admin.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.contrib import admin
-from todo.models import Project
-
-class ProjectAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Project, ProjectAdmin)
